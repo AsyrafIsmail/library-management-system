a8e59e2f535d0e29526b425fe25cccd7610642b9
Asyraf Ismail <nikasyraf268@gmail.com>
2026-05-11 13:52:10 +0800

    feat: add relationships

--- app/Models/BorrowRequest.php
+++ app/Models/BorrowRequest.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class BorrowRequest extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
 }
